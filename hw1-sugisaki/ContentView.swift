86f372bd75af0ec5f2cec15291a974c28a9463b3
00657106-ios <aa00657106@gmail.com>
2020-10-07 17:41:07 +0800

    Initial Commit

---
+++ hw1-sugisaki/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  hw1-sugisaki
+//
+//  Created by User04 on 2020/10/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
